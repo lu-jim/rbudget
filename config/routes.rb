2de2073c387cb0adabfe00b090d6f4c1497e5c98
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-29 18:05:05 -0400

    Create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => ''
+  get 'pages/home'
+  devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
 end
