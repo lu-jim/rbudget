ac0f3b4d00a45645d300720c492a095d3316b9c4
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-30 12:50:29 -0400

    Create categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
   get 'pages/home'
   devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
