c1d5f95e6557393319d9bc23c6c8363de575ed50
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-30 13:21:53 -0400

    Prepare routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'pages/home'
   devise_for :users, path: ''
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  resources :groups, only: %i[index show new delete]
+  resources :log_records, only: %i[index show new delete]
 
   # Defines the root path route ("/")
   root 'pages#home'
