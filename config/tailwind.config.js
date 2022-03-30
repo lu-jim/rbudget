0df2596ba1fb2e311534d92c2bc5fd301034dda1
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-29 20:21:47 -0400

    Add font customizations to tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,28 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
+  content: ['./app/helpers/**/*.rb', './app/javascript/**/*.js', './app/views/**/*.{erb,haml,html,slim}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'snap-main': '#3778c2',
-      'snap-sec': '#60B524',
-      'snap-text': '#434C55',
-    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        body: ['Raleway', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'snap-main': '#3778c2',
+        'snap-sec': '#60B524',
+        'snap-text': '#434C55',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
+};
