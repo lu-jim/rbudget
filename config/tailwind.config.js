e8aea6df2a62b7e1178128f6fdd9f73a431ddd5a
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-29 23:29:20 -0400

    Customize home logo

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         body: ['Raleway', ...defaultTheme.fontFamily.sans],
+        display: ['Merriweather', ...defaultTheme.fontFamily.serif],
       },
       colors: {
         'snap-main': '#3778c2',
         'snap-sec': '#60B524',
         'snap-text': '#434C55',
+        'snap-title': '#3D4550',
       },
     },
   },
