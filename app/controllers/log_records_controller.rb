959e9a0cb4f9b286352079e3337525f9268aea80
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-30 22:13:02 -0400

    Create base controller logic

--- app/controllers/log_records_controller.rb
+++ app/controllers/log_records_controller.rb
@@ -1,10 +1,17 @@
 class LogRecordsController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @log_record = LogRecord.new
+  end
+
   def index
+    @current_user = current_user
+    @log_records = @current_user.log_records.includes([:author])
   end
 
   def show
-  end
-
-  def new
+    @current_user = current_user
+    @log_record = @current_user.log_record.find(params[:log_record_id])
   end
 end
