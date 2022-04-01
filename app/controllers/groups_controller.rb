4340196af5223442774c417c7a61af82ecbbb28d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-01 15:22:12 -0400

    Configure group#create and update routing

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,4 +11,18 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find_by_id(params[:id])
   end
+
+  def create
+    @user = current_user
+    @group = @user.groups.new(group_params)
+    if @group.save
+      redirect_to groups_path, flash: { success: 'Group created successfully.' }
+    else
+      render :new, flash: { danger: @group.errors.messages }
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
