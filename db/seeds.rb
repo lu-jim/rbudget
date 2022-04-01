9ff3c7fdfdd777344add5731ee47756fb51ccf9d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-01 10:52:55 -0400

    Delete reduntant files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 user1 = User.create(name: 'Luma', email: 'luma@luma.xyz', password: '123123', password_confirmation: '123123')
-category1 = Group.create(name: 'Groceries', icon: 'https://cdn-icons-png.flaticon.com/512/3050/3050209.png', author: user1)
+category1 = Group.create(name: 'Groceries',
+                         icon: 'https://cdn-icons-png.flaticon.com/512/3050/3050209.png', author: user1)
 category2 = Group.create(name: 'Credit', icon: 'https://cdn-icons-png.flaticon.com/512/147/147258.png', author: user1)
 transaction1 = category1.log_records.create(author: user1, name: 'plaza', amount: 20)
 transaction1.groups.push(category2)
