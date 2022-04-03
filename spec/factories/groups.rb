79ba3ef19d81f155e24e933871af41b3c54a0aee
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-02 22:08:24 -0400

    Fix linting errors

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :group do
     name { Faker::Fantasy::Tolkien.location }
     image_data { TestData.image_data }
-    author_id { Faker::Number.within(range: 1..10)}
+    author_id { Faker::Number.within(range: 1..10) }
   end
 end
