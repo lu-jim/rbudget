f8bbcd055712ffd59fb1bcf370ec26059aebffe6
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-02 20:25:51 -0400

    Add transaction card

--- app/controllers/log_records_controller.rb
+++ app/controllers/log_records_controller.rb
@@ -14,18 +14,17 @@ class LogRecordsController < ApplicationController
 
   def create
     @group = Group.find_by_id(params[:group_id])
-    @groups = current_user.groups
 
-    if params[:groups].nil?
-      flash[:fail] = 'Please select at least 1 category'
-      return redirect_to new_log_record_path(group_id: @group)
+    log_record_params.each do |param|
+      if param.nil?
+        flash[:fail] = 'This field is mandatory'
+        return redirect_to new_log_record_path(group_id: @group)
+      end
     end
-
     @log_record = current_user.log_records.new(log_record_params)
-
     if @log_record.save
-      sanitized_params = params[:groups].reject(&:empty?)
-      sanitized_params.each do |g|
+      sanitized_groups = params['groups']
+      sanitized_groups.each do |g|
         @log_record.groups << Group.find(g)
       end
       redirect_to group_path(@group), flash: { success: 'Transaction created successfully.' }
