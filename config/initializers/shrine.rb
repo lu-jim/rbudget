ed241f55480efe08d5dfb116977a2d42e709bf7d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-02 21:57:27 -0400

    Fix shrine test setup

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,10 +2,17 @@ require 'shrine'
 require 'shrine/storage/file_system'
 require 'shrine/storage/memory'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
-}
+Shrine.storages = if Rails.env.test?
+                    {
+                      cache: Shrine::Storage::Memory.new,
+                      store: Shrine::Storage::Memory.new
+                    }
+                  else
+                    {
+                      cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
+                      store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
+                    }
+                  end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
